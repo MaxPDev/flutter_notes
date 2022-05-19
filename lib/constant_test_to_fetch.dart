b096f3706752df43fe72104e410f7f53ab5389ac
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2022-05-19 17:21:24 +0200

    parking card

--- lib/constant_test_to_fetch.dart
+++ lib/constant_test_to_fetch.dart
@@ -3,11 +3,19 @@ import 'package:flutter/material.dart';
 const titre = "Parkings";
 
 //# name
-const pkgName = "Parkings";
-const pkgLetterSpaceName = 2.0;
-const pkgColor = Colors.grey;
+const parkingTitle = "Parkings";
+const horaireTitle = "Horaires";
+const letterSpaceName = 2.0;
+const titleColor = Colors.grey;
+
+const dataColor = Colors.amberAccent;
+const datafontSize = 28.0;
 
 //# data
-const pkg1Color = Colors.amberAccent;
 
 const pkg1Name = "Gare Thiers";
+const horaire1 = "Ouvert 24h/7";
+
+const placePkg1 = '26 places disponibles';
+const placeFontSize = 18.0;
+const placeLetterSpacing = 1.0;
