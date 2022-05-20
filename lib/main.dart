fe65a1c72efbc614f66671eb9b7ee1c88e47b34f
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2022-05-20 15:22:50 +0200

    setup location app. new: SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:fr_piscadev_reminder/models/quote.dart';
-import 'package:fr_piscadev_reminder/Widgets/quote_card.dart';
+import 'package:fr_piscadev_reminder/screens/home.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: QuoteList(),
+    home: SafeArea(
+      child: Home()
+      ),
   ));
 }
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({Key? key}) : super(key: key);
-
-//# State object
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-
-  List<Quote> quotes = [
-    Quote(author: "Max", text: "Flutter's constructor are not bad"),
-    Quote(author: "Nancy", text: "I wan't that app quickly and cheap"),
-    Quote(author: "Dupond", text: "I have nothing to say exepct what I just said"),
-  ];
-
-  //# Créer une fonction qui retourne un template, prend un quote en param, utilise Card
-  //? Type return could be Card ?
-  // Widget quoteTemplate(quote){
-  //   return QuoteCard(quote: quote);
-  // }
-//# Plutôt que créer cette fonction, on peut créer le widget correspondant. Exctrating Widgets.
-//# No more usefull : use Widget instead
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-
-      appBar: AppBar(
-        title: const Text('Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-
-      body: Column(
-
-        children: 
-
-        quotes.map((quote) => QuoteCard(
-          quote: quote,
-          delete: () {
-            setState(() {
-              //# remove is a function of List that it's called when delete function is call in the child
-              quotes.remove(quote);
-            });
-          }
-          )).toList(), //# {} because . : var.prop
-      )
-    );
-  }
-}
-
